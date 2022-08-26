f19bd0c833951dd755e27d054ed01ae0c79cff3b
Bonheur Bushiri <bonheurbushiri@gmail.com>
2022-08-26 13:54:51 +0200

    renome de ranche

--- src/Components/MonApi.js
+++ src/Components/MonApi.js
@@ -4,9 +4,9 @@ import React, { useState } from 'react'
 function MonApi (){
     
     const getApi = ()=>{
-        fetch('https://api.openweathermap.org/data/2.5/forecast?q=goma&appid=f5586608c2d3ba7b09e56baef9ba3d46&units=imperial')
+        fetch('https://api.openweathermap.org/data/2.5/forecast?q=goma&appid=f5586608c2d3ba7b09e56baef9ba3d46&units=metric&lang=fr_cn')
         .then(response => response.json())
-        .then(json => console.log(json.list))
+        .then(json => console.log(json.list[2]))
 
     }
             console.log(getApi())
